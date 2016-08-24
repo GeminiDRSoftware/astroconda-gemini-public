d846b1fa73bfd98309a85bb849ae82ecd9624b34
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2016-08-23 20:18:33 -0400

    Remove or silence 2to3 execution (#55)

--- pyraf/build.sh
+++ pyraf/build.sh
@@ -5,7 +5,11 @@ fi
 
 if [[ $PY3K > 0 ]]; then
 set +e
-    2to3 -w .
+    echo Running 2to3...
+    2to3 -w . &>/dev/null
+    if [[ $? != 0 ]]; then
+        echo "ERROR: 2to3 technically failed!"
+    fi
 fi
 
 python setup.py install || exit 1
