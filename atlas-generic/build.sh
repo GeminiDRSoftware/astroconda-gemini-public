aaab79452a41d9c6e6932782c2405d65f91cf54c
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2016-11-10 13:29:56 -0500

    Atlas fixes (#104)
    * Update atlas-generic so it builds
    
    * Fix quoted OPTIONS

--- atlas-generic/build.sh
+++ atlas-generic/build.sh
@@ -1,23 +1,20 @@
-
-
 export LIBTOOL=/usr/bin/libtool
 export OPTIONS=
 
 case "$(uname -s)" in
-Darwin)
-export CC=/opt/local/bin/gcc-mp-4.8
-export F77=/opt/local/bin/gfortran-mp-4.8
-export OPTIONS="-C agc $CC -C if $F77 --shared -t 2 -b 64 -V 128 -A 12"
-;;
-Linux)
-export CC=/usr/bin/gcc
-export F77=/usr/bin/gfortran
-export OPTIONS="-C agc $CC -C if $F77 --shared -t 2 -b 64 -V 128 -A 12"
-;;
-*)
-;;
+    Darwin)
+        export CC=/opt/local/bin/gcc-mp-4.8
+        export F77=/opt/local/bin/gfortran-mp-4.8
+    ;;
+    Linux)
+        export CC=/usr/bin/gcc
+        export F77=/usr/bin/gfortran
+    ;;
+    *)
+    ;;
 esac
 
+export OPTIONS="-Fa alg -fPIC -C agc $CC -C if $F77 --shared -t 2 -b 64 -V 128 -A 12"
 export LAPACK_VERSION=3.5.0
 export LAPACK_TARBALL=lapack-${LAPACK_VERSION}.tgz
 export LAPACK_URL="http://www.netlib.org/lapack/${LAPACK_TARBALL}"
@@ -26,10 +23,10 @@ wget ${LAPACK_URL}
 mkdir -p build
 pushd ./build
 ../configure --prefix=$PREFIX \
---with-netlib-lapack-tarfile=../${LAPACK_TARBALL} \
-"${OPTIONS}"
+    --with-netlib-lapack-tarfile=../${LAPACK_TARBALL} \
+    ${OPTIONS}
 
-make LIBTOOL=$LIBTOOL
+make LIBTOOL=$LIBTOOL CC=$CC F77=$F77
 make install
 
 popd
